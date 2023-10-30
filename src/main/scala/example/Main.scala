a186177d22fb342e902ae83a4e2c836ca733c40d
yurique <i@yurique.com>
2023-10-30 13:43:18 +0100

    smaller

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,13 +1,7 @@
 package example
 
 object Main extends App with Test {
-
-  case class MyData()
-  case class MyData2()
-
-  load(MyData())()
-  load(MyData2())()
-
+  load("")()
 }
 
 trait Test {
